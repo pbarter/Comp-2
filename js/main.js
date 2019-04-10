88b0bff4bfc67ed937b08f3f1b99e2fcf40215f4
Patrick B <patrick.barter@rogers.com>
2019-04-10 19:16:14 -0400

    Edited in nav, and weekly schedule with HTML and CSS. Also small JS code for dropdown

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
