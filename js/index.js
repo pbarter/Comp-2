8d27697672f901d73c3793b1019b42fbf908183f
Patrick B <patrick.barter@rogers.com>
2019-04-28 16:56:19 -0400

    Fixed the index.html and js not updating (may have been my fault)

--- js/index.js
+++ js/index.js
@@ -7,17 +7,14 @@ let password = 1234
 const unId = document.getElementById(`un`);
 const pwId = document.getElementById(`pw`);
 const loginButton = document.getElementById(`login`);
-const enterKey = document.getElementById(`loginform`)
-
 
 //Function to get JS to match input with our username and password
 loginButton.addEventListener(`click`, event => {
     if (unId.value == username, pwId.value == password) {
-        alert(`Welcome ${username}, loggin you in!`);
         unId.value = ``
         pwId.value = ``
         window.location.href = "dashboard.html";
-
+        console.log(`works`)
     }
 // If the username or password inputs do not match with what I have as the username and password, do this:
     else {
